b33899274e43bf25ef6d8f23b97c065e29619be8
Marius Nicolae <marius.nicolae@microchip.com>
2018-11-02 14:14:18 +0200

    add new files

---
+++ ATmega4809_TCB_Examples/Using_TCB_in_Time-Out_Check_Mode/main.c
@@ -0,0 +1,102 @@
+/**
+ * \file main.c
+ *
+ * \brief Main source file.
+ *
+ (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use this software and
+    any derivatives exclusively with Microchip products. It is your responsibility
+    to comply with third party license terms applicable to your use of third party
+    software (including open source software) that may accompany Microchip software.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void CLOCK_init (void)
+{
+    /* Enable writing to protected register */
+    CPU_CCP = CCP_IOREG_gc;
+    /* Disable CLK_PER Prescaler */
+    CLKCTRL.MCLKCTRLB = 0 << CLKCTRL_PEN_bp;
+    
+    /* Enable writing to protected register */
+    CPU_CCP = CCP_IOREG_gc;
+    /* Select 32KHz Internal Ultra Low Power Oscillator (OSCULP32K) */
+    CLKCTRL.MCLKCTRLA = CLKCTRL_CLKSEL_OSCULP32K_gc;
+   
+    /* Wait for system oscillator changing to finish */
+    while (CLKCTRL.MCLKSTATUS & CLKCTRL_SOSC_bm)
+    {
+        ;
+    }
+}
+
+void PORT_init (void)
+{
+    PORTB.DIR |= PIN5_bm; /* Configure PB5 as digital output */
+    PORTB.OUT |= PIN5_bm; /* Set initial level of PB5 */
+    PORTB.DIR &= ~(PIN2_bm); /* Configure PB2 as digital input */
+    PORTB.PIN2CTRL = PORT_PULLUPEN_bm; /* Enable the internal pullup */
+}
+
+void EVENT_SYSTEM_init (void)
+{
+    EVSYS.CHANNEL0 = EVSYS_GENERATOR_PORT1_PIN2_gc; /* Port 1 Pin 2 */
+    EVSYS.USERTCB0 = EVSYS_CHANNEL_CHANNEL0_gc; /* Connect user to event channel 0 */
+}
+
+void TCB0_init (void)
+{
+    /* Load the Compare or Capture register with the timeout value*/
+    TCB0.CCMP = 0x7fff;
+    
+    /* Enable TCB and set CLK_PER divider to 1 (No Prescaling) */
+    TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm;
+    
+    /* Configure TCB in Periodic Timeout mode */
+    TCB0.CTRLB = TCB_CNTMODE_TIMEOUT_gc;
+    
+    /* Enable Capture or Timeout interrupt */
+    TCB0.INTCTRL = TCB_CAPT_bm;
+    
+    /* Enable Event Input and Event Edge*/
+    TCB0.EVCTRL = TCB_CAPTEI_bm | TCB_EDGE_bm;
+}
+
+ISR(TCB0_INT_vect)
+{
+    TCB0.INTFLAGS = TCB_CAPT_bm; /* Clear the interrupt flag */
+    PORTB.IN = PIN5_bm; /* Toggle PB5 GPIO */
+}
+
+int main(void)
+{
+    CLOCK_init();
+    PORT_init();
+    EVENT_SYSTEM_init();
+    TCB0_init();
+
+    sei(); /* Enable Global Interrupts */
+    
+    while (1)
+    {
+        ;
+    }        
+}
+
