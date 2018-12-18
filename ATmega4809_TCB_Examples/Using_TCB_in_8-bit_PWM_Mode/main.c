b58680ed3a8c0fd2c15f02f12a01850005a0bb5b
Marius Nicolae <marius.nicolae@microchip.com>
2018-12-18 16:57:53 +0200

    Version 1.0

---
+++ ATmega4809_TCB_Examples/Using_TCB_in_8-bit_PWM_Mode/main.c
@@ -0,0 +1,85 @@
+/**
+ * \file main.c
+ *
+ * \brief Main source file.
+ *
+ (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use this software and
+    any derivatives exclusively with Microchip products. It is your responsibility
+    to comply with third party license terms applicable to your use of third party
+    software (including open source software) that may accompany Microchip software.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
+#include <avr/io.h>
+
+#define TCB_CMP_EXAMPLE_VALUE   (0x80FF)
+
+void CLOCK_init (void);
+void PORT_init (void);
+void TCB3_init (void);
+
+void CLOCK_init (void)
+{
+    /* Enable writing to protected register */
+    CPU_CCP = CCP_IOREG_gc;
+    /* Enable Prescaler and set Prescaler Division to 64 */
+    CLKCTRL.MCLKCTRLB = CLKCTRL_PDIV_64X_gc | CLKCTRL_PEN_bm;
+    
+    /* Enable writing to protected register */
+    CPU_CCP = CCP_IOREG_gc;
+    /* Select 32KHz Internal Ultra Low Power Oscillator (OSCULP32K) */
+    CLKCTRL.MCLKCTRLA = CLKCTRL_CLKSEL_OSCULP32K_gc;
+    
+    /* Wait for system oscillator changing to finish */
+    while (CLKCTRL.MCLKSTATUS & CLKCTRL_SOSC_bm)
+    {
+        ;
+    }
+}
+
+void PORT_init (void)
+{
+    PORTB_DIR |= PIN5_bm;
+    PORTB_OUT |= PIN5_bm;
+}
+
+void TCB3_init (void)
+{
+    /* Load CCMP register with the period and duty cycle of the PWM */
+    TCB3.CCMP = TCB_CMP_EXAMPLE_VALUE;
+
+    /* Enable TCB3 and Divide CLK_PER by 2 */
+    TCB3.CTRLA |= TCB_ENABLE_bm;
+    TCB3.CTRLA |= TCB_CLKSEL_CLKDIV2_gc;
+    
+    /* Enable Pin Output and configure TCB in 8-bit PWM mode */
+    TCB3.CTRLB |= TCB_CCMPEN_bm;
+    TCB3.CTRLB |= TCB_CNTMODE_PWM8_gc;
+}
+
+int main(void)
+{
+    CLOCK_init();
+    PORT_init();
+    TCB3_init();
+    
+    while (1)
+    {
+        ;
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+ATmega4809_TCB_Examples
+
+This repository contains examples of bare metal source code for TCB as described in TB3214 document from Microchip. The repository contains an Atmel Studio Solution with multiple projects inside. Make sure you have the necessary tools:
+
+    1. ATmega4809 device (or the ATmega4809 X-plained PRO evaluation board)
+    2. Atmel Studio
+    3. ATmega4809 Device Packs
+
+The following examples are inside this repository:
+
+    1. TCB_Sleep_Mode_Operation
+	2. Using_TCB_in_8-bit_PWM_Mode
+	3. Using_TCB_in_Time-Out_Check_Mode
